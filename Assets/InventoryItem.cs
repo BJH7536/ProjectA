ccc0b36259d2c0ddbf4bdd43eccb8b89e7cc57bf
AerideS <82868023+AerideS@users.noreply.github.com>
2024-01-24 11:22:28 +0900

    [ADD] Stackable 아이템 기능 추가

--- Assets/InventoryItem.cs
+++ Assets/InventoryItem.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public Item item;
-
     [Header("UI")]
     public Image image;
+    public Text countText;
+
+    [HideInInspector] public Item item;
+
+    // 쌓을 수 있는 아이템을 표시하기 위해 사용
+    [HideInInspector] public int count = 1;
 
     [HideInInspector] public Transform parentAfterDrag;
 
@@ -17,27 +22,33 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     {
         item = newItem;
         image.sprite = newItem.image;
+        RefreshCount();
+    }
+
+    // 아이템 수량 리플레쉬
+    public void RefreshCount()
+    {
+        countText.text = count.ToString();
+
+        bool textActive = count > 1;
+        countText.gameObject.SetActive(textActive);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("Begin drag");
+        image.raycastTarget = false;
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
-        transform.SetAsLastSibling();
-        image.raycastTarget = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("Dragging");
         transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("End drag");
-        transform.SetParent(parentAfterDrag);
         image.raycastTarget = true;
+        transform.SetParent(parentAfterDrag);
     }
 }
--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    // 최대 중첩 횟수
+    public int maxStackedItems = 4;
     public InventorySlot[] inventorySlots;
     public GameObject inventoryItemPrefab;
 
     public bool AddItem(Item item)
     {
+        // 모든 아이템 슬롯을 대상으로 진행
+        for(int i = 0; i < inventorySlots.Length; i++)
+        {
+            // i 번째 InventorySlot을 가져와서
+            InventorySlot slot = inventorySlots[i];
+            // 해당 Slot에 있는 InventoryItem을 가져옴
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+
+            // 가져온 아이템이 null이 아니고, 같은 아이템이고, 갯수가 4보다 작다면
+            // 아이템 갯수 증가
+            if (itemInSlot != null &&
+                itemInSlot.item == item &&
+                itemInSlot.count < maxStackedItems)
+            {
+                itemInSlot.count++;
+                itemInSlot.RefreshCount();
+                return true;
+            }
+        }
+
         for(int i = 0; i < inventorySlots.Length; i++)
         {
             InventorySlot slot = inventorySlots[i];
