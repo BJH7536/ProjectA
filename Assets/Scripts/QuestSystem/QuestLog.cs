9503946f6e0b628cf644f9b7724f2dcf6bf5c6eb
lemonmungchi <khc112353@gmail.com>
2024-03-20 12:09:08 +0900

    퀘스트

--- Assets/Scripts/QuestSystem/QuestLog.cs
+++ Assets/Scripts/QuestSystem/QuestLog.cs
@@ -9,5 +9,6 @@ public class QuestLog : MonoBehaviour
     public QuestManager quest;                //전체 패널에 붙이고 state를 채크해서 additem으로 지금 수행하고있는 퀘스트를 가져온다. 
     public Button[] QuestList;                //퀘스트 리스트 
 
-
+    public TextMeshProUGUI QuestTitle;
+    public TextMeshProUGUI QuestDescription;
 }
