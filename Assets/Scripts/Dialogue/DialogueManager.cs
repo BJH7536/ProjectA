63e90478be4208a389e861054c14d2fc65f5e1b8
lemonmungchi <khc112353@gmail.com>
2024-03-21 11:58:07 +0900

    1
    1

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -4,7 +4,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 public class DialogueManager : MonoBehaviour
@@ -92,5 +94,119 @@ public class DialogueManager : MonoBehaviour
     {
         return portraitData[id + portraitIndex];
     }
+
+    //public void EnterDialogueMode(TextAsset inkJSON)
+    //{
+    //    currentStory = new Story(inkJSON.text);
+    //    dialogueIsPlaying = true;
+    //    dialoguePanel.SetActive(true);
+
+    //    //태그 초기화
+    //    displayNameText.text = "???";
+    //    portraitAnimator.Play("default");
+    //    layoutAnimator.Play("right");
+
+    //    ContinueStory();
+    //}
+
+    //private void ExitDialogueMode()
+    //{
+    //    dialogueIsPlaying = false;
+    //    dialoguePanel.SetActive(false);
+    //    dialogueText.text = "";
+    //}
+
+    //private void ContinueStory()
+    //{
+    //    if (currentStory.canContinue)                   //더 보여줄 이야기가 있다면
+    //    {
+    //        dialogueText.text = currentStory.Continue();            //한줄 출력
+    //        DisplayChoices();                                       //선택이 있으면 선택출력
+    //        //태그관리
+    //        HandleTags(currentStory.currentTags);
+    //    }
+    //    else
+    //    {
+    //        ExitDialogueMode();
+    //    }
+    //}
+
+    //private void HandleTags(List<string> currentTags)
+    //{
+    //    foreach (string tag in currentTags)
+    //    {
+    //        string[] splitTag = tag.Split(':');
+    //        if (splitTag.Length != 2)
+    //        {
+    //            Debug.LogError("Tag parsed error : " + tag);
+    //        }
+    //        string tagkey = splitTag[0].Trim();
+    //        string tagvalue = splitTag[1].Trim();
+
+    //        switch (tagkey)
+    //        {
+    //            case SPEAKER_TAG:
+    //                displayNameText.text = tagvalue;
+    //                break;
+    //            case PORTRAIT_TAG:
+    //                portraitAnimator.Play(tagvalue);
+    //                break;
+    //            case LAYOUT_TAG:
+    //                layoutAnimator.Play(tagvalue);
+    //                break;
+    //            default:
+    //                Debug.LogWarning("Tag exists but not handled");
+    //                break;
+    //        }
+
+    //    }
+    //}
+
+    //private void DisplayChoices()
+    //{
+    //    List<Choice> currentChoices = currentStory.currentChoices;
+
+    //    if (currentChoices.Count > choices.Length)           //현재 선택지의 개수가 버튼의 개수보다 많으면 오류 
+    //    {
+    //        Debug.LogError("More choices than ever");
+    //    }
+
+    //    int index = 0;
+    //    foreach (Choice choice in currentChoices)
+    //    {
+    //        choices[index].gameObject.SetActive(true);
+    //        choicesText[index].text = choice.text;
+    //        index++;
+    //    }
+
+    //    for (int i = index; i < choices.Length; i++)
+    //    {
+    //        choices[i].gameObject.SetActive(false);
+    //    }
+
+    //    StartCoroutine(SelectFirstChoice());
+    //}
+
+    //private IEnumerator SelectFirstChoice()
+    //{
+    //    EventSystem.current.SetSelectedGameObject(null);
+    //    yield return new WaitForEndOfFrame();
+    //    EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    //}
+
+    //public void MakeChoice(int choiceIndex, string npcId)
+    //{
+    //    currentStory.ChooseChoiceIndex(choiceIndex);
+    //    Debug.Log(npcId);
+    //    if (choiceIndex == 0 && npcId.Contains("Quest") && start)
+    //    {
+    //        //NPC id 와 Quest ID를 일치시켜야함
+    //        Managers.Questevent.StartQuest(npcId);
+    //    }
+    //    else if (choiceIndex == 0 && npcId.Contains("Quest") && end)
+    //    {
+    //        Managers.Questevent.FinishQuest(npcId);
+    //    }
+    //}
 
 }
